7ef567d7ded1d20c99a4dd4bce38681658e7efd6
nouenthary <tharynouen22@gmail.com>
2024-03-21 14:46:36 +0700

    add fb plugin

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -1617,10 +1617,28 @@
 
 
 			<div id="fb-root"></div>
-
-			<div id="fb-customer-chat" class="fb-customerchat"></div>
-
 			<script>
+				(function(d, s, id) {
+					var js, fjs = d.getElementsByTagName(s)[0];
+					if (d.getElementById(id)) return;
+					js = d.createElement(s);
+					js.id = id;
+					js.src = 'https://connect.facebook.net/fi_FI/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';
+					fjs.parentNode.insertBefore(js, fjs);
+				}(document, 'script', 'facebook-jssdk'));
+			</script>
+
+			<!-- Your customer chat code -->
+			<div class="fb-customerchat" attribution=setup_tool page_id="1160695934081727" greeting_dialog_display="hide">
+			</div>
+
+
+			<!-- <div id="fb-root"></div> -->
+
+
+			<!-- <div id="fb-customer-chat" class="fb-customerchat"></div> -->
+
+			<!-- <script>
 				var chatbox = document.getElementById('fb-customer-chat');
 				chatbox.setAttribute("page_id", "106008288957042");
 				chatbox.setAttribute("attribution", "biz_inbox");
@@ -1644,7 +1662,7 @@
 					js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
 					fjs.parentNode.insertBefore(js, fjs);
 				}(document, 'script', 'facebook-jssdk'));
-			</script>
+			</script> -->
 
 			<!--**********************************
             Footer start
